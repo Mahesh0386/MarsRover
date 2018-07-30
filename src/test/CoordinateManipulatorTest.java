c94241d6d29d965c9f11419e44f0442765be0334
Mahesh0386 <mahesh.bhannur@gmail.com>
2018-07-30 10:56:41 -0400

    Add remaining tests for  CoordinateManipulatorTest.

--- src/test/CoordinateManipulatorTest.java
+++ src/test/CoordinateManipulatorTest.java
@@ -2,21 +2,55 @@ package test;
 
 import main.CoordinateManipulator;
 import main.Coordinates;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CoordinateManipulatorTest {
 
+    private Coordinates currentCoordinates;
+    private CoordinateManipulator coordinateManipulator;
+    private Coordinates resultantCoordinates;
+
+
+    @Before
+    public void setUp()
+    {
+        currentCoordinates = new Coordinates(1,1);
+        coordinateManipulator = new CoordinateManipulator();
+    }
+
     @Test
     public void decrementXCoordinate_shouldDecrementXCoordinate()
     {
-        Coordinates currentCoordinates = new Coordinates(1,1);
-        CoordinateManipulator coordinateManipulator = new CoordinateManipulator();
-        Coordinates resultantCoordinates = coordinateManipulator.decrementXCoordinate(currentCoordinates);
-
+        resultantCoordinates = coordinateManipulator.decrementXCoordinate(currentCoordinates);
         assertEquals(0,resultantCoordinates.getX());
         assertEquals(1,resultantCoordinates.getY());
+    }
+
+    @Test
+    public void incrementXCoordinate_shouldIncrementXCoordinate()
+    {
+        Coordinates resultantCoordinates = coordinateManipulator.incrementXCoordinate(currentCoordinates);
+        assertEquals(2,resultantCoordinates.getX());
+        assertEquals(1,resultantCoordinates.getY());
+    }
+
+    @Test
+    public void decrementYCoordinate_shouldDecrementYCoordinate()
+    {
+        Coordinates resultantCoordinates = coordinateManipulator.decrementYCoordinate(currentCoordinates);
+        assertEquals(1,resultantCoordinates.getX());
+        assertEquals(0,resultantCoordinates.getY());
+    }
+
+    @Test
+    public void incrementYCoordinate_shouldIncrementYCoordinate()
+    {
+        Coordinates resultantCoordinates = coordinateManipulator.incrementYCoordinate(currentCoordinates);
+        assertEquals(1,resultantCoordinates.getX());
+        assertEquals(2,resultantCoordinates.getY());
     }
 
 }
